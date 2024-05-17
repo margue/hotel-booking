5fc87e3389b71944103a30fdb6969f1e517bfef2
Martin Günther <martin.guenther@holisticon.de>
2024-05-17 15:44:05 +0200

    docs: add overview

--- src/main/java/service/BookingRequestInterval.java
+++ src/main/java/service/BookingRequestInterval.java
@@ -2,5 +2,8 @@ package service;
 
 import java.time.LocalDate;
 
+// BookingRequestInterval ist sicher keine Domänensprache?
+// Braucht es an dieser Stelle überhaupt so ein küsntliches Objekt?
+// Reichen nicht ArrivalDate und DepartureDate
 public record BookingRequestInterval(LocalDate startDate, LocalDate endDate) {
 }
