fd4ce3f63ed30d73f8109c6a52d44f6b9d4ce68d
Nicole Rauch <info@nicole-rauch.de>
2024-08-15 10:22:24 +0200

    Leider viel Boilerplate, aber klareres Interface für Either.

--- src/main/java/service/Either.java
+++ src/main/java/service/Either.java
@@ -1,5 +1,16 @@
 package service;
 
-public record Either<T, U>(T error, U result) {
+public class Either<T, U> {
+    private final T error;
+    private final U result;
+    private Either(T error, U result) {
+        this.error = error;
+        this.result = result;
+    }
+    public T error() { return error; }
+    public U result() { return result; }
+
+    public static <T, U> Either<T, U> ofError(T error) { return new Either<>(error, null); }
+    public static <T, U> Either<T, U> ofResult(U result) { return new Either<>(null, result); }
     public boolean isError(){ return error != null; }
 }
