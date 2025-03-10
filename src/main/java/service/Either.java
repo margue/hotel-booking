9714dd870791acc1fc0c01d76376a6013e6f2038
Nicole Rauch <info@nicole-rauch.de>
2025-03-10 12:09:18 +0100

    Either als abstrakte Klasse mit Subklassen statt Verwendung von null.

--- src/main/java/service/Either.java
+++ src/main/java/service/Either.java
@@ -1,9 +1,29 @@
 package service;
 
-public record Either<T, U>(T error, U result) {
+public abstract class Either<T, U> {
 
-    public boolean isError(){ return error != null; }
+    public abstract boolean isError();
+    public T error(){ throw new UnsupportedOperationException(); };
+    public U result(){ throw new UnsupportedOperationException(); }
 
-    public static <T, U> Either<T, U> ofError(T error) { return new Either<>(error, null); }
-    public static <T, U> Either<T, U> ofResult(U result) { return new Either<>(null, result); }
+    public static <T, U> Either<T, U> ofError(T error) { return new Left<>(error); }
+    public static <T, U> Either<T, U> ofResult(U result) { return new Right<>(result); }
+}
+
+class Right<U> extends Either {
+    private final U result;
+    public Right(U result) { this.result = result; }
+    @Override
+    public U result() { return this.result; }
+    @Override
+    public boolean isError() { return false; }
+}
+
+class Left<T> extends Either {
+    private final T error;
+    public Left(T error) { this.error = error; }
+    @Override
+    public T error() { return this.error; }
+    @Override
+    public boolean isError() { return true; }
 }
